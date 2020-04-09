94adfd9950acdea28cfd4bc40221846a19b96332
Dmitri <dimirisov@gmail.com>
2020-04-09 19:11:35 +0300

    yoga tabs

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function () { // script's work start after loading DOM elements correctly
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'), // connect to HTML elements with DOM
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) { //creating of func which hide all emelemnts in <div info-tabcontent></div>
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    };
+    hideTabContent(1); //this function is hiding every element in .info-tabcontent , so we put (1) as argument to save first element
+
+    function showTabContent(b) { // 
+        if (tabContent[b].classList.contains('hide')) { //this function created to show elements 
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    };
+
+    info.addEventListener('click', function (event) {// creating of event
+        if (target && target.classList.contains('info-header-tab')) { // condition checking 
+            for (let i = 0; i < tab.length; i++) { // this cycle connects tab which is clicked with element which is showed after click
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
+
